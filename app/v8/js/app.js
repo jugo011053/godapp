809155c5d3a94c557f9050a703bc48d55348c435
jugo011053 <janik.duratovic99@gmail.com>
2026-08-05 18:21:41 +0200

    Install integrated recipe and shopping enhancements

--- app/v8/js/app.js
+++ app/v8/js/app.js
@@ -2,6 +2,7 @@ import { emit, on } from './core/events.js';
 import { getRoute, initializeRouter } from './core/router.js';
 import { getState, initializeStore, updateState } from './core/store.js';
 import { rerenderIntegratedApp, startIntegratedApp } from './integrationController.js';
+import { installFeatureEnhancements } from './featureEnhancements.js';
 
 const appRoot = document.getElementById('app');
 
@@ -10,6 +11,7 @@ on('route:changed', () => rerenderIntegratedApp(appRoot));
 on('state:changed', () => rerenderIntegratedApp(appRoot));
 initializeRouter();
 await startIntegratedApp(appRoot);
+await installFeatureEnhancements(appRoot);
 
 window.PreplyV8 = Object.freeze({
   getState,
